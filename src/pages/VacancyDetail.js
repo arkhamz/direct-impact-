a018976b5e75ddb0e44465002674d6b3ca0b224a
arkhamz <shile2007@gmail.com>
2022-10-14 11:39:50 +0200

    detail page done

--- src/pages/VacancyDetail.js
+++ src/pages/VacancyDetail.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import "./VacancyDetail.css"
 import { useParams } from "react-router-dom";
 import { useVacancyContext } from "../context/config";
+import { specFormatter, tagRemover } from "../utils";
+import {v4 as uuidv4} from "uuid"
 
 
 function VacancyDetail(){
@@ -9,28 +11,43 @@ function VacancyDetail(){
     const {id} = useParams();
     const {state,dispatch,setDetail,clearDetail} = useVacancyContext();
     const vacancy = state.vacancyDetail;
+    const specsFormatted = vacancy && specFormatter(vacancy.specs);
 
     useEffect(function(){
 
         // thunk gets detail by id, dispatches to state
         setDetail(id);
 
-
+        //cleanup function, removes vacancy detail on unmount
         return function(){
             clearDetail();
         }
     }, []);
     
     return <section className="vacancy-detail">
-        <div className="detail-intro">
-            {vacancy && vacancy.title}
-
+        {vacancy && (
+            <>
+            <div className="detail-intro">
+            <h1 className="vacancy-title">{vacancy.title}</h1>
+                <div className="detail-specs">
+                {specsFormatted && specsFormatted.map(function(i){
+                    return <span key={uuidv4()} >{i}</span>
+                })}
+                </div>
         </div>
-        <div className="detail-description"></div>
-        <div className="recruiter"></div>
-        <div className="apply">
-            <button>Apply</button>
-        </div>
+            <div className="detail-description">
+                <h2 className="vacancy-description">Description</h2>
+                <p className="description-text">{tagRemover(vacancy.intro)}</p>
+            </div>
+            <div className="recruiter">
+                <h3>Recruiter:</h3>
+                <p>{vacancy.recruiter}</p>
+            </div>
+             <div className="apply">
+                <button>Apply</button>
+            </div>
+            </>
+        )}
 
     </section>
 }
